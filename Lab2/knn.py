21b0a1e037af659d33fc65e30f73cf85f89645f8
Boynton <boyntonrl@ad.msoe.edu>
2019-12-12 00:59:47 -0600

    Add lab 2 files

---
+++ Lab2/knn.py
@@ -0,0 +1,45 @@
+import random
+
+import numpy as np
+from scipy import spatial
+from scipy import stats
+
+class KNN:
+    """
+    Implementation of the k-nearest neighbors algorithm for classification
+    and regression problems.
+    """
+    def __init__(self, k, aggregation_function):
+        """
+        Takes two parameters.  k is the number of nearest neighbors to use
+        to predict the output variable's value for a query point. The
+        aggregation_function is either "mode" for classification or
+        "average" for regression.
+        """
+        self.k = k
+
+        if aggregation_function == 'mode' or 'average':
+            self.aggregation_function = aggregation_function 
+        else:
+            raise ValueError('aggregation_function must be either "mode" or "average"')
+        
+        self.X = None
+        self.y = None
+        
+
+    def fit(self, X, y):
+        """
+        Stores the reference points (X) and their known output values (y).
+        """
+        self.X = X
+        self.y = y
+
+        
+        
+    def predict(self, X):
+        """
+        Predicts the output variable's values for the query points X.
+        """
+        
+        
+        
