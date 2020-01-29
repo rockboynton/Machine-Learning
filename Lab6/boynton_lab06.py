0e3ae5a9a5ab0d7760418e44ef702640ac2ba5b0
Rock Boynton <boyntonrl@msoe.edu>
2020-01-29 11:52:17 -0600

    Add jupyter nb as python script to repo

---
+++ Lab6/boynton_lab06.py
@@ -0,0 +1,50 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # Continued Cost Function Fun
+# 
+# Rock Boynton | CS 4850
+# 
+# ## Introduction
+# 
+# In this lab, we will get more practice writing cost functions, implement the
+# linear regression machine learning algorithm and explore the effect of feature
+# selection.
+#
+# The two cost functions we will be implementing are gaussian and multivariate
+# linear regression
+#
+# 
+# We will then run the following experiments on the linear regression model to make
+# predictions on an advertising dataset
+# 
+# 1. Numerical Approximation
+#
+# 2. Normal Solutions
+# 
+# ## Summary of Results
+# 
+#
+# ---
+
+# %% [markdown]
+# ## Setup
+# 1. Load in the Advertisement.csv dataset and identify the column pertaining to the response
+# variable and the feature variables.
+#
+# 2. Rescale each of the features such that the values range between 0 and 1.
+# 
+# 3. Add a feature to the dataset to include information about the baseline/DC term. This feature
+# should be a constant among all observations.
+#
+# 4. At the end of this step you should have one variable (numpy matrix) that holds the feature
+# matrix and one variable (numpy vector) that holds the response.
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('Advertising.csv', delimiter=',', skiprows=1, usecols=[1, 2, 3])
+
+
+# %%
